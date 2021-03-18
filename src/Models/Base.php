4778b171ccc6c120126f04429e4cc236f2c7bd26
kakhura <giokaxura@gmail.com>
2021-03-18 22:06:22 +0400

    create model db connection

--- src/Models/Base.php
+++ src/Models/Base.php
@@ -6,4 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Base extends Model
 {
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->setConnection(config(sprintf('kakhura.site-bases.models_connection_mapper.%s', get_called_class()), 'mysql'));
+    }
 }
