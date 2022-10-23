9d1eda970ee6b8d2dd331d485f59fd1ace046f78
Giorgi Kakhurashvili <kakhura@Giorgis-MacBook-Pro-1.local>
2022-10-23 22:26:22 +0400

    change ordering into base model

--- src/Models/Base.php
+++ src/Models/Base.php
@@ -3,6 +3,7 @@
 namespace Kakhura\LaravelSiteBases\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Base extends Model
 {
@@ -10,5 +11,17 @@ class Base extends Model
     {
         parent::__construct($attributes);
         $this->setConnection(config(sprintf('kakhura.site-bases.models_connection_mapper.%s', get_called_class()), 'mysql'));
+    }
+
+    public static function boot()
+    {
+        static::updating(function (self $model) {
+            if ($model->isDirty('ordering') && !Str::contains(url()->current(), 'ordering')) {
+                $model->update([
+                    'ordering' => 0,
+                ]);
+            }
+        });
+        parent::boot();
     }
 }
