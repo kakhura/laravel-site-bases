44d17c3edb941c3c88d5bb2d0213f47eb9f0195d
Giorgi Kakhurashvili <kakhura@Giorgis-MacBook-Pro-1.local>
2022-10-26 00:12:52 +0400

    remove boot

--- src/Models/Base.php
+++ src/Models/Base.php
@@ -3,7 +3,6 @@
 namespace Kakhura\LaravelSiteBases\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
 class Base extends Model
 {
@@ -11,17 +10,5 @@ class Base extends Model
     {
         parent::__construct($attributes);
         $this->setConnection(config(sprintf('kakhura.site-bases.models_connection_mapper.%s', get_called_class()), 'mysql'));
-    }
-
-    public static function boot()
-    {
-        static::updating(function (self $model) {
-            if ($model->isDirty('ordering') && !Str::contains(url()->current(), 'ordering')) {
-                $model->update([
-                    'ordering' => 0,
-                ]);
-            }
-        });
-        parent::boot();
     }
 }
