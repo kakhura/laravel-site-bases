af9d29af77e3dc5f6d248baea20fe0bc35fd8ae0
Giorgi Kakhurashvili <kakhura@Giorgis-MacBook-Pro-1.local>
2023-11-02 23:04:13 +0400

    change for detail

--- src/Traits/Models/ForDetail.php
+++ src/Traits/Models/ForDetail.php
@@ -6,21 +6,21 @@ trait ForDetail
 {
     public function getTitleAttribute()
     {
-        return $this->detail->first()->title;
+        return $this->detail->first() ? $this->detail->first()->title : '';
     }
 
     public function getDescriptionAttribute()
     {
-        return $this->detail->first()->description;
+        return $this->detail->first() ? $this->detail->first()->description : '';
     }
 
     public function getDescriptionMinAttribute()
     {
-        return $this->detail->first()->description_min;
+        return $this->detail->first() ? $this->detail->first()->description_min : '';
     }
 
     public function getAddressAttribute()
     {
-        return $this->detail->first()->address;
+        return $this->detail->first() ? $this->detail->first()->address : '';
     }
 }
